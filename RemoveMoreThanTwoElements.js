eee31dc1d27cd9c7bce8cf99de67913f1dd2fc4d
ganeshdvp <ganeshcherupalli6565@gmail.com>
2025-04-14 21:12:04 +0530

    initial commit

---
+++ RemoveMoreThanTwoElements.js
@@ -0,0 +1,16 @@
+let arr = [0,0,0,1,2,2,2,2,2,3,3,4];  // o/p: [0,0,1,2,2,3,3,4]
+
+let newArray = [];
+for(let i=0; i<arr.length; i++){
+    let count =0 ;
+    for(let j=0; j<newArray.length; j++){
+        if(arr[i] == newArray[j]){
+            count++;
+        }
+    }
+    if(count<2){
+        newArray.push(arr[i]);
+    }
+}
+console.log(newArray);
+//time and space complexity:- O(n*2) and (n)
