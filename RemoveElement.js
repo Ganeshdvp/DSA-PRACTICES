69618bfe8a357f04910001517154438aee856e37
ganeshdvp <ganeshcherupalli6565@gmail.com>
2025-04-16 21:37:43 +0530

    initial commit

--- RemoveElement.js
+++ RemoveElement.js
@@ -1,4 +1,4 @@
-let arr = [0,0,1,2,2,2,3,4,0];  //[0,1,2,3,4]
+let arr = [0, 0, 1, 2, 2, 2, 3, 4, 0]; //[0,1,2,3,4]
 
 //Approach 1
 
@@ -8,18 +8,29 @@ let arr = [0,0,1,2,2,2,3,4,0];  //[0,1,2,3,4]
 
 //Approach 2
 
-let newArray =[];
+// let newArray = [];
 
-for(let i=0; i<arr.length; i++){
-    let count = 0;
-    for(let j=0; j<newArray.length; j++){
-        if(arr[i] == newArray[j]){
-            count++;
-        }
-    }
-    if(count === 0){
-        newArray.push(arr[i])
-    }
-}
-console.log(newArray);
+// for (let i = 0; i < arr.length; i++) {
+//   let count = 0;
+//   for (let j = 0; j < newArray.length; j++) {
+//     if (arr[i] == newArray[j]) {
+//       count++;
+//     }
+//   }
+//   if (count === 0) {
+//     newArray.push(arr[i]);
+//   }
+// }
+// console.log(newArray);
 //Time and Space complexity :- O(n*2) and O(n)
+
+//Approach 3
+
+let k = 1;
+for (let i = 1; i < arr.length; i++) {
+  if (arr[i] != arr[k - 1]) {
+    arr[k] = arr[i];
+    k++;
+  }
+}
+console.log(arr.slice(0, k-1)); // [0,1,2,3,4]
